ffcdb1f90c45750e2547edcc593381061b8f73cf
oskarvedel <oskar.vedel@gmail.com>
2020-03-15 16:05:03 +0100

    new changes

---
+++ The_Debt_Book/The_Debt_Book/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using The_Debt_Book;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;s
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using System.Xml.Serialization;
+
+namespace The_Debt_Book
+{
+	public class MainWindowViewModel
+	{
+		private ObservableCollection<Debtors> Debtor;
+	}
+		public MainWindowViewModel()
+		{
+
+		}
+
+	}
