b53294045d5f4fb52ea41a37a6911fa59eab0db9
oskarvedel <oskar.vedel@gmail.com>
2020-03-15 16:09:11 +0100

    ahaaa

--- The_Debt_Book/The_Debt_Book/MainWindowViewModel.cs
+++ The_Debt_Book/The_Debt_Book/MainWindowViewModel.cs
@@ -1,24 +1,12 @@
-﻿using The_Debt_Book;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.IO;s
-using System.Runtime.CompilerServices;
-using System.Windows;
-using System.Windows.Input;
-using System.Windows.Threading;
-using System.Xml.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace The_Debt_Book
 {
-	public class MainWindowViewModel
+	class MainWindowViewModel
 	{
-		private ObservableCollection<Debtors> Debtor;
 	}
-		public MainWindowViewModel()
-		{
-
-		}
-
-	}
+}
