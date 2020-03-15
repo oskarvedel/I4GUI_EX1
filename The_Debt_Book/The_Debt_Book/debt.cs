8571f4cf4d28b3e8f75dd04759852c10017d5031
oskarvedel <oskar.vedel@gmail.com>
2020-03-15 18:21:50 +0100

    added debt class

---
+++ The_Debt_Book/The_Debt_Book/debt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Debt_Book
+{
+	class Debt
+	{
+		private int value;
+		private DateTime date;
+
+		public Debt(int dvalue, DateTime ddate)
+		{
+			value = dvalue;
+			date = ddate;
+		}
+		public int Debtvalue
+		{
+			get { return value; }
+			set { value = value; }
+		}
+		public DateTime datevalue
+		{
+			get { return date; }
+
+			set { date = value; }
+		}
+	}
+}
