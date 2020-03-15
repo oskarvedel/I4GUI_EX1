2291e536d0755928347ee76b5d252cb6bd28f7be
oskarvedel <oskar.vedel@gmail.com>
2020-03-15 15:00:34 +0100

    lavede nye ting

--- The_Debt_Book/The_Debt_Book/Debt.cs
+++ The_Debt_Book/The_Debt_Book/Debt.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Prism.Mvvm;
 
 namespace The_Debt_Book
 {
-	private ObservableCollection<Agent> agents;
-    public class DebtBookViewModel : BindableBase
-    {
-        
-    }
+	class Debt
+	{
+	}
 }
