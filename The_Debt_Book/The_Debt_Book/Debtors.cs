0df614597279e0031118050ba1cd212055e0f87e
SBrahe <201807173@post.au.dk>
2020-03-15 16:02:50 +0100

    added debtor class

---
+++ The_Debt_Book/The_Debt_Book/Debtors.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Debt_Book
+{
+    class Debtors
+    {
+        private string name;
+        private int value;
+
+        public Debtors()
+        {
+        }
+
+        public Debtors(string dname, int dvalue)
+        {
+            name = dname;
+            value = dvalue;
+
+        }
+
+        public string Debtorname
+        {
+            get { return name; }
+
+            set { name = value; }
+        }
+
+        public int Debtvalue
+        {
+            get { return value; }
+
+            set { value = value; }
+        }
+        
+    }
+}
