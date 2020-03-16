c3f8e9966c505c176a92c2cb659ff2d5cf0058ff
SBrahe <201807173@post.au.dk>
2020-03-16 15:43:08 +0100

    Add  virker igen

--- The_Debt_Book/The_Debt_Book/ViewModels/MainWindowViewModel.cs
+++ The_Debt_Book/The_Debt_Book/ViewModels/MainWindowViewModel.cs
@@ -70,6 +70,7 @@ namespace The_Debt_Book
                     var newDebtor = new Debtor();
                     var vm = new AddDebtorViewModel(newDebtor);
                     var win2 = new AddDebtorsWindow();
+                    win2.DataContext = vm;
                     if (win2.ShowDialog() == true)
                     {
                         debtorList.Add(newDebtor);
